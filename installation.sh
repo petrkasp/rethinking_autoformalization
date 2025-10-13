0c014ca7075b7e331c29f277acd44ba1a6b89e59
Petr Kašpárek <petr@kasparek.cz>
2025-10-13 13:24:20 +0200

    More installation

--- installation.sh
+++ installation.sh
@@ -2,7 +2,6 @@ python3 -m venv .env
 .env/bin/pip install -r requirements.txt
 
 curl https://elan.lean-lang.org/elan-init.sh -sSf | sh
-1
 source $HOME/.elan/env
 elan toolchain install 4.7.0-rc2
 lake update
